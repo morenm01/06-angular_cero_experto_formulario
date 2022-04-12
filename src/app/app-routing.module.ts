7d452c4badfafda1ef71bede26014cd4abe3f6f6
miriam <m.moreno.clavijo@gmail.com>
2022-04-12 16:19:43 +0200

    add templates and reactives modules and lazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveModule } from './reactive/reactive.module';
 
-const routes: Routes =[];
+const routes: Routes =[
+  {
+    path: 'reactivo',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule)
+  },
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( m => m.TemplateModule)
+  }
+];
 
 
 @NgModule({
